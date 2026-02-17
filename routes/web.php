625021702337f7a1cbfbae294e9bb86f98145ed0
Gabriel <contacto.loyal1a@gmail.com>
2026-02-17 19:28:47 +0000

    Cambios gelia defecto

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,20 @@ use App\Http\Controllers\GeliaController;
 
 // 1. Cuando entres a la página principal, te mostrará la pantalla web (la Vista)
 Route::get('/', function () {
-    return view('resurtido');
+    return view('gelia');
 });
 
 // --- RUTAS DE GELIA (Versión 2.0) ---
-Route::get('/gelia', function () {
-    return view('gelia');
-})->name('gelia.vista');
+//Route::get('/gelia', function () {
+//    return view('gelia');
+//})->name('gelia.vista');
 
 //Cuando le des clic al botón de "Generar" en el formulario, 
 // esta ruta recibe los archivos y se los manda al Cerebro (el Controlador)
 //Route::post('/generar', [ResurtidoController::class, 'generar'])->name('resurtido.generar');
 
 // Cambia la ruta POST para usar el FastController
-Route::post('/generar', [FastResurtidoController::class, 'generar'])->name('resurtido.generar');
+//Route::post('/generar', [FastResurtidoController::class, 'generar'])->name('resurtido.generar');
 
 
 // Ruta para generar
