bb2926db6eb66170c04d8058338f9314796145aa
Gabriel <contacto.loyal1a@gmail.com>
2026-05-08 11:13:12 -0600

    Mejora, actualizacion refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -4,16 +4,19 @@
 
 echo "🚀 Iniciando actualización del sistema GELIA..."
 
+echo "Descargando últimos cambios del repositorio..."
+git pull origin main
+
 # 1. Compilar assets de frontend (Vite)
 echo "📦 Compilando módulos JS y CSS..."
-docker-compose -f compose.prod.yaml exec gelia npm run build
+docker compose -f compose.prod.yaml exec gelia npm run build
 
 # 2. Limpiar caché profundo de Laravel
 echo "🧹 Limpiando caché (Config, Vistas, Rutas)..."
-docker-compose -f compose.prod.yaml exec gelia php artisan optimize:clear
+docker compose -f compose.prod.yaml exec gelia php artisan optimize:clear
 
 # 3. Reconstruir caché optimizado
 echo "⚡ Generando caché de producción..."
-docker-compose -f compose.prod.yaml exec gelia php artisan optimize
+docker compose -f compose.prod.yaml exec gelia php artisan optimize
 
 echo "✅ Actualización completada con éxito."
